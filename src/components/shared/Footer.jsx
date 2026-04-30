d135779cbe8e058b114db37380cae543a1ac3c7c
raf0670 <swagbondhu@gmail.com>
2026-04-30 17:56:41 +0600

    navbar and footer completed

---
+++ src/components/shared/Footer.jsx
@@ -0,0 +1,80 @@
+import { Mail, MapPin, Phone } from 'lucide-react';
+import React from 'react';
+import { FaFacebook, FaGithub, FaInstagram, FaTwitter } from 'react-icons/fa';
+
+const Footer = () => {
+    return (
+        <footer className="bg-[#0a0a0b] text-gray-300 border-t border-gray-100">
+            <div className="mx-auto max-w-7xl px-4 py-12 sm:px-6 lg:px-8">
+                <div className="grid grid-cols-1 gap-8 md:grid-cols-3">
+
+                    <div className='flex flex-col justify-center items-center'>
+                        <h3 className="text-sm font-semibold uppercase tracking-wider text-gray-300">
+                            Contact Us
+                        </h3>
+                        <ul className="mt-4 space-y-3">
+                            <li className="flex items-center gap-3 text-gray-300">
+                                <MapPin className="size-5 text-purple-500" />
+                                <span className="text-sm">Dhaka, Bangladesh</span>
+                            </li>
+                            <li className="flex items-center gap-3 text-gray-300">
+                                <Phone className="size-5 text-pink-500" />
+                                <span className="text-sm">+880 1234 567 890</span>
+                            </li>
+                            <li className="flex items-center gap-3 text-gray-300">
+                                <Mail className="size-5 text-orange-500" />
+                                <span className="text-sm">support@yourstore.com</span>
+                            </li>
+                        </ul>
+                    </div>
+
+                    <div className="flex flex-col justify-center items-center md:items-center">
+                        <h3 className="text-sm font-semibold uppercase tracking-wider text-gray-300">
+                            Follow Our Journey
+                        </h3>
+                        <div className="mt-4 flex gap-5">
+                            <a href="#" className="p-2 rounded-full bg-linear-to-r from-indigo-500 via-purple-500 to-orange-400 hover:bg-linear-to-br hover:from-indigo-500 hover:via-purple-500 hover:to-orange-400 hover:text-white transition-all duration-300">
+                                <FaFacebook className="size-5" />
+                            </a>
+                            <a href="#" className="p-2 rounded-full bg-linear-to-r from-indigo-500 via-purple-500 to-orange-400 hover:bg-linear-to-br hover:from-indigo-500 hover:via-purple-500 hover:to-orange-400 hover:text-white transition-all duration-300">
+                                <FaInstagram className="size-5" />
+                            </a>
+                            <a href="#" className="p-2 rounded-full bg-linear-to-r from-indigo-500 via-purple-500 to-orange-400 hover:bg-linear-to-br hover:from-indigo-500 hover:via-purple-500 hover:to-orange-400 hover:text-white transition-all duration-300">
+                                <FaTwitter className="size-5" />
+                            </a>
+                            <a href="#" className="p-2 rounded-full bg-linear-to-r from-indigo-500 via-purple-500 to-orange-400 hover:bg-linear-to-br hover:from-indigo-500 hover:via-purple-500 hover:to-orange-400 hover:text-white transition-all duration-300">
+                                <FaGithub className="size-5" />
+                            </a>
+                        </div>
+                    </div>
+
+                    <div className="md:text-right flex flex-col justify-center items-center">
+                        <h3 className="text-sm font-semibold uppercase tracking-wider text-gray-300">
+                            Legal
+                        </h3>
+                        <nav className="mt-4 flex flex-col gap-2 text-center">
+                            <a href="#" className="text-sm text-gray-300 hover:text-purple-600 transition">Privacy Policy</a>
+                            <a href="#" className="text-sm text-gray-300 hover:text-purple-600 transition">Terms of Service</a>
+                            <a href="#" className="text-sm text-gray-300 hover:text-purple-600 transition">Cookie Policy</a>
+                        </nav>
+                    </div>
+                </div>
+
+                {/* Bottom Bar */}
+                <div className="mt-12 border-t border-gray-100 pt-8 flex flex-col md:flex-row justify-between items-center gap-4">
+                    <p className="text-xs text-gray-500">
+                        &copy; {new Date().getFullYear()} YourStore. All rights reserved.
+                    </p>
+                    <div className="flex items-center gap-1">
+                        <span className="text-xs text-gray-400">Powered by</span>
+                        <span className="text-xs font-bold bg-linear-to-r from-indigo-500 via-purple-500 to-orange-400 bg-clip-text text-transparent">
+                            Sun Cart
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
---
+++ src/components/shared/Navbar.jsx
@@ -0,0 +1,68 @@
+import Image from 'next/image';
+import React from 'react';
+import sunsetLogo from "@/assets/tropicalsunset.png";
+import Link from 'next/link';
+
+const Navbar = () => {
+    return (
+        <header className="shadow-sm">
+            <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div className="flex h-16 items-center justify-between">
+                    <div className="md:flex md:items-center md:gap-12">
+                        <Link className="flex gap-3 items-center justify-center w-full" href={"/"}>
+                            <Image src={sunsetLogo} alt='' width={10} height={10} className='w-full h-full rounded-full'></Image>
+                            <h1 className='text-3xl font-bold bg-linear-to-r from-indigo-500 via-purple-500 to-orange-400 bg-clip-text text-transparent'>SunCart</h1>
+                        </Link>
+                    </div>
+
+                    <div className="hidden md:block">
+                        <nav aria-label="Global">
+                            <ul className="flex items-center gap-6 text-md font-semibold">
+                                <li><Link href={"/"}>Home</Link></li>
+                                <li><Link href={"/products"}>Products</Link></li>
+                                <li><Link href={"/my-profile"}>My Profile</Link></li>
+                            </ul>
+                        </nav>
+                    </div>
+
+                    <div className="flex items-center gap-4">
+                        <div className="sm:flex sm:gap-4">
+                            <Link
+                                className="rounded-md bg-linear-to-r from-indigo-500 via-purple-500 to-orange-400 px-5 py-2.5 text-sm font-medium text-white shadow-lg transition hover:brightness-110 hover:shadow-purple-500/25"
+                                href={"/login"}
+                            >
+                                Login
+                            </Link>
+
+                            <div className="hidden sm:flex">
+                                <Link
+                                    className="group relative inline-block p-0.5 rounded-md bg-linear-to-r from-indigo-500 via-purple-500 to-orange-400"
+                                    href={"/register"}
+                                >
+                                    <span className="block rounded-md bg-white px-5 py-2.5 text-sm font-medium text-gray-700 transition group-hover:bg-transparent group-hover:text-white">
+                                        Register
+                                    </span>
+                                </Link>
+                            </div>
+                        </div>
+
+                        <div className="dropdown dropdown-end block md:hidden">
+                            <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                                <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h7" /> </svg>
+                            </div>
+                            <ul
+                                tabIndex="-1"
+                                className="menu menu-lg dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+                                <li><Link href={"/"}>Home</Link></li>
+                                <li><Link href={"/products"}>Products</Link></li>
+                                <li><Link href={"/my-profile"}>My Profile</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+};
+
+export default Navbar;
